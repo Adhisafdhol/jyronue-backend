2f3dda39fecd8567b1627504372e71afec06b1db
Aadheuss <aadheusx@gmail.com>
2024-09-15 13:06:23 +0700

    Set up passport local strategy on app.js

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const passportConfig = require("./config/passportConfig");
+passport.use(new LocalStrategy(passportConfig.strategy));
+passport.serializeUser(passportConfig.serializeUser);
+passport.deserializeUser(passportConfig.deserializeUser);
+
 const indexRouter = require("./routes/index");
 const userRouter = require("./routes/user");
 
