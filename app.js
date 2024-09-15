44b0e27b0c929b40d90b20dfc62b5e57dc93cc0c
Aadheuss <aadheusx@gmail.com>
2024-09-15 12:56:29 +0700

    Change router name from usersRouter to userRouter

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/user");
+const userRouter = require("./routes/user");
 
 const app = express();
 
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
-app.use("/user", usersRouter);
+app.use("/user", userRouter);
 
 module.exports = app;
