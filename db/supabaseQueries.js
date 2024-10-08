992a0ca26f21d143e4cf47b74bdfab30a68dab47
Aadheuss <aadheusx@gmail.com>
2024-10-09 00:50:02 +0700

    Add supabasedb queries to upload and get url

---
+++ db/supabaseQueries.js
@@ -0,0 +1,32 @@
+const supabase = require("../config/supabaseConfig");
+const { decode } = require("base64-arraybuffer");
+
+exports.uploadFile = async ({ user, file, from }) => {
+  // Upload file using standard upload
+  const fileBase64 = decode(file.buffer.toString("base64"));
+  const folder = user.id;
+
+  const { data, error } = await supabase.storage
+    .from(from)
+    .upload(`${folder}/${file.originalname}`, fileBase64, {
+      contentType: file.mimetype,
+    });
+
+  if (error) {
+    throw new Error("Failed to upload file");
+  } else {
+    return data;
+  }
+};
+
+exports.getPublicUrl = ({ file, from }) => {
+  const { data, error } = supabase.storage
+    .from(from)
+    .getPublicUrl(file.originalname);
+
+  if (error) {
+    throw new Error("Failed to get public url");
+  } else {
+    return data;
+  }
+};
