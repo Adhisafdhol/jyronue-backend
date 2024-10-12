c2aef805c77342a47a9bd97542d22e43fc1908f5
Aadheuss <aadheusx@gmail.com>
2024-10-12 14:32:25 +0700

    Add function to check if string is UUID

--- utils/utils.js
+++ utils/utils.js
@@ -8,3 +8,12 @@ exports.convertFileName = (file) => {
 
   return { ...file, originalname: newName };
 };
+
+exports.isUUID = (value) => {
+  const regex = new RegExp(
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/,
+    "i"
+  );
+
+  return regex.test(value);
+};
